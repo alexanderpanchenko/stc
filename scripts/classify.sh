82edbd1731f7d92376cc155a9160f4051d364cab
Alexander Panchenko <panchenko.alexander@gmail.com>
2012-11-13 12:26:08 +0100

    classify

--- scripts/classify.sh
+++ scripts/classify.sh
@@ -3,16 +3,17 @@
 # Process the command line arguments
 if [ $# -lt 2 ] ; then
 	echo "This script classifies a set of texts with a pre-trained model."
-	echo "Usage: classify.sh <input-file> <model-dir>"  
-	echo "<input-file> An XML file in the TCS format to clasify (not changed)"
-	echo "<model-dir> A directry with a classification model"
+	echo "Usage: classify.sh <input-file> <model-dir> <expansion-max>"  
+	echo "<input-file> XML file in the TCS format to clasify (not changed)"
+	echo "<model-dir> Directry with a classification model"
+	echo "<expansion-max> Maximum number of expansions"
 	exit 0
 fi
 
 # Set the parameters
 file=$1
 model=$2
-stc=/home/sasha/work/active/icop/src/icop-console/Release/stc
+stc=./../bin/stc
 
 if [ $# -lt 3 ] ; then
 	expansion_max=0
@@ -26,3 +27,6 @@ cp $file $copy
 
 # Classify the copy of the file
 $stc -M p -i $copy -m $model/model -V $model/vocabulary.csv -s $model/stopwords.csv -c $model/stopos.csv -N u -r $model/relations.csv -E $expansion_max 
+
+# Remove the copy
+rm $copy
